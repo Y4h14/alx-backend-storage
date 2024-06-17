10daeae872e594ca95e98516629f0a162aa57715
Y4h14 <yahyasadig2013@gmail.com>
2024-06-17 11:42:47 +0300

    updated print format

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,21 +6,21 @@ if __name__ == '__main__':
 
     client = MongoClient()
     nginx_collection = client.logs.nginx
-    
+
     logs_num = nginx_collection.count_documents({})
-    print(f'{logs_num} logs')
+    print('{} logs'.format(logs_num))
     print('Methods:')
     get = nginx_collection.count_documents({'method': 'GET'})
     post = nginx_collection.count_documents({'method': 'POST'})
     put = nginx_collection.count_documents({'method': 'PUT'})
     patch = nginx_collection.count_documents({'method': 'PATCH'})
     delete = nginx_collection.count_documents({'method': 'DELETE'})
-    
-    print(f'    method GET: {get}')
-    print(f'    method POST: {post}')
-    print(f'    method PUT: {put}')
-    print(f'    method PATCH: {patch}')
-    print(f'    method DELETE: {delete}')
-    
+
+    print('    method GET: {}'.format(get))
+    print('    method POST: {}'.format(post))
+    print('    method PUT: {}'.format(put))
+    print('    method PATCH: {}'.format(patch))
+    print('    method DELETE: {}'.format(delete))
+
     status = nginx_collection.count_documents({"method": 'GET', "path": '/status'})
-    print(f'{status} status check')
+    print('{} status check'.format(status))
