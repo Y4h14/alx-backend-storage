0d964765f4d25ee132400239dbdcc78de903b094
Y4h14 <yahyasadig2013@gmail.com>
2024-06-19 17:37:22 +0300

    task 0 (initial commit)

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""defines a Cache class"""
+import redis
+import uuid
+
+
+class Cache:
+    """
+    A class for creating a cahce
+    """
+    def __init__(self) -> None:
+        """
+        initalize the redis cache
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+
+    def store(self, data):
+        """
+        creates a key for the data, and store the data on redis
+        Parameters:
+        data (Any): the data to be stored
+
+        Returns:
+        (string) : the key where the passed data is stored
+        """
+        key_id = str(uuid.uuid4())
+        self._redis.set(key_id, data)
+
+        return key_id
+
